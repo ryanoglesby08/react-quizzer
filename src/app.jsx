886e0f5657c9a7ea771a1b0ada1bf450f6f2493e
Ryan Oglesby <ryan.oglesby08@gmail.com>
2016-06-29 19:21:47 -0500

    Use state to capture the user's answer and display whether they were right or wrong

--- src/app.jsx
+++ src/app.jsx
@@ -3,25 +3,84 @@ import React from 'react'
 
 const question = require('./question.json');
 
-class Answer extends React.Component {
+class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+    this.setGuess = this.setGuess.bind(this);
+  }
+
+  setGuess(answer) {
+    console.log(`Chosen answer = ${answer}`);
+
+    this.setState({guess: answer});
+  }
+
   render() {
-    return <li>{this.props.text}</li>
+    return (
+      <div>
+        <Question question={this.props.question} guess={this.state.guess} chooseAnswer={this.setGuess}/>
+      </div>
+    );
   }
 }
 
 class Question extends React.Component {
   render() {
-    return <div>
-              <h2>{this.props.question.title}</h2>
-              <ul>
+    return (
+      <section>
+        <h2>{this.props.question.title}</h2>
+        <ul>
+          {this.props.question.answers.map((name) =>
+            <Answer key={name}
+                    text={name}
+                    isGuessed={name === this.props.guess}
+                    isCorrect={name === this.props.question.correct}
+                    chooseAnswer={this.props.chooseAnswer}/>
+          )}
+        </ul>
+      </section>
+    );
+  }
+}
 
-                {this.props.question.answers.map((answer) => <Answer key={Math.random()} text={answer} />)}
-              </ul>
-          </div>
+class Answer extends React.Component {
+  render() {
+    const handleClick = () => this.props.chooseAnswer(this.props.text);
+
+    const displayGuess = (isGuessed, isCorrect) => {
+      if (!isGuessed) {
+        return "";
+      }
+
+      return isCorrect ? " Correct!" : " Wrong!"
+    };
+
+    return (
+      <li onClick={handleClick}>
+        {this.props.text}
+        <span>
+          {displayGuess(this.props.isGuessed, this.props.isCorrect)}
+        </span>
+      </li>
+    );
   }
 }
 
 ReactDOM.render(
-  <Question question={question} />,
+  <Quiz question={question} />,
   document.getElementById("app")
 );
+
+
+
+//{
+//  "title": "Who created React?",
+//  "answers": [
+//    {"name": "Facebook", "correct": true},
+//    {"name": "Twitter"},
+//    {"name": "Amazon"},
+//    {"name": "Netflix"}
+//  ]
+//}
