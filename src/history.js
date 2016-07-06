63aba4d87c3dd554bda025f26e5c7af0df432bb1
Ryan Oglesby <ryan.oglesby08@gmail.com>
2016-07-05 19:04:24 -0500

    Moved each history line into a class to better handle the key property

--- src/history.js
+++ src/history.js
@@ -13,36 +13,38 @@ const styles = {
   }
 };
 
+class HistoryLine extends React.Component {
+  render() {
+    const answer = this.props.answer;
+
+    const correctAnswerText = <p>Answer: {answer.guess}</p>
+    const wrongAnswerText =
+      <div>
+        <p>Your Answer: {answer.guess}</p>
+        <p>Correct Answer: {answer.correct}</p>
+      </div>;
+
+    return (
+      <li style={answer.hasBeenGuessedCorrectly() ? styles.correct : styles.incorrect}>
+        <h2>
+          <span className="badge">{answer.hasBeenGuessedCorrectly() ? 'right!' : 'wrong'}</span>
+          {answer.title}
+        </h2>
+        {answer.hasBeenGuessedCorrectly() ? correctAnswerText : wrongAnswerText}
+      </li>
+    );
+  }
+}
+HistoryLine.propTypes = {
+  answer: React.PropTypes.object.isRequired
+};
+
 export default class History extends React.Component {
   render() {
-    const stylesFor = (answer) => answer.hasBeenGuessedCorrectly() ? styles.correct : styles.incorrect;
-
     return (
       <section className="history">
         <ul>
-          {
-            this.props.history.map((answer) =>
-              <li key={answer.title} style={stylesFor(answer)}>
-                <h2>
-                  <span className="badge">{answer.hasBeenGuessedCorrectly() ? 'right!' : 'wrong'}</span>
-                  {answer.title}
-                </h2>
-                {(() => {
-                  if (answer.hasBeenGuessedCorrectly()) {
-                    return <p>Answer: {answer.guess}</p>;
-                  }
-                  else {
-                    return (
-                      <div>
-                        <p>Your Answer: {answer.guess}</p>
-                        <p>Correct Answer: {answer.correct}</p>
-                      </div>
-                    );
-                  }
-                })()}
-              </li>
-            )
-          }
+          {this.props.history.map((answer) => <HistoryLine key={answer.title} answer={answer}/>)}
         </ul>
       </section>
     );
